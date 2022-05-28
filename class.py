72435d3eada56270d70e644a461dc0bbb58b025d
aaliyanah56 <rahmaadannoor@gmail.com>
2022-05-28 22:23:30 +0300

    class.py

--- class.py
+++ class.py
@@ -7,12 +7,12 @@ def __init__(self, Name, age, tracks, score):
         self.age = age
         self.tracks = tracks
         self.score = score
-        
+    #create the first object
     #object of student
 class Student:
     Name = "Bob"
     age = 34
-    tracks = "full stack"
+    tracks = "UI/UX"
     score = 10.11
     print('class student: ')
     print('Name is a', Name)
@@ -20,17 +20,5 @@ class Student:
     print('track is a', tracks)
     print('score is a', score)
 
-    #class variables can be accessed using class
-    #name also
-    #print("\nAccessing class variable using class name")
-    #print(SelfReg.name)
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
 print("\nAccessing class variable using class name")
 print(SelfReg.name)
